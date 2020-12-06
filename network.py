c2cbb60ffb6c5bd197baf4d4049cecfba7af92cd
arun <arun@layeriv.com>
2020-12-06 16:07:24 +0530

    code refactored with numpy

--- network.py
+++ network.py
@@ -1,19 +1,13 @@
-def w_sum(weights, inputs):
-    assert(len(weights) == len(inputs))
-    output = 0
-    for i in range(len(inputs)):
-        output += weights[i] * inputs[i]
-    return output
-    
+import numpy as np
+
 def neural_network(weights, inputs):
-    prediction  = w_sum(weights, inputs)
+    prediction  = inputs.dot(weights)
     return prediction
 
-toes = [8.5, 9.5, 10, 9]
-wins_loss = [0.65, 0.8, 0.75, 0.6]
-num_fans = [1.2 , 1.1, 1.4, 5.3]
-
-weights = [0.1, 0.2, 0]
-inputs = [toes[0], wins_loss[0], num_fans[0]]
+toes = np.array([8.5, 9.5, 10, 9])
+wins_loss = np.array([0.65, 0.8, 0.75, 0.6])
+num_fans = np.array([1.2 , 1.1, 1.4, 5.3])
+weights = np.array([0.1, 0.2, 0])
+inputs = np.array([toes[0], wins_loss[0], num_fans[0]])
 prediction = neural_network(weights, inputs)
 print(prediction)
