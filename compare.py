8cedf59ffd33506e27c673b7cc72a8bd35bd334c
arun <arun@layeriv.com>
2020-12-06 19:43:07 +0530

    cool, able to see power of tiny alph guy!!

--- compare.py
+++ compare.py
@@ -1,11 +1,12 @@
 weight, goal_of_prediction, input = (0.0, 0.8, 2)
+alpha = 0.1
 
-for iteration in range(10):
+for iteration in range(20):
     print('----------\nWeight', weight)
     prediction = weight * input
     error = (prediction - goal_of_prediction)**2
     delta = prediction - goal_of_prediction
     weight_delta = delta * input
-    weight = weight - weight_delta
+    weight = weight - weight_delta * alpha
     print('Error', error, ' Prediction', prediction)
     print('Delta', str(delta), ' Weight Delta', weight_delta)
