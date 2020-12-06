3d27ddec4cf07349ba7622315ad2e1da53d6398b
arun <arun@layeriv.com>
2020-12-06 19:00:00 +0530

    log prediction, error, delta, weight delta

--- compare.py
+++ compare.py
@@ -1,17 +1,11 @@
-weight = 0.1
-alpha = 0.01
+weight, goal_of_prediction, input = (0.0, 0.8, 1.1)
 
-
-num_of_toes = [8.5]
-win_or_lose_binary = [1]
-
-input = num_of_toes[0]
-goal_prediction = win_or_lose_binary[0]
-
-for iteration in range(100):
-    prediction = input * weight
-    error = (prediction - goal_prediction)**2
-    print('Error', str(error), 'Prediction', prediction)
-    delta = prediction - goal_prediction
+for iteration in range(4):
+    print('----------\nWeight', weight)
+    prediction = weight * input
+    error = (prediction - goal_of_prediction)**2
+    delta = prediction - goal_of_prediction
     weight_delta = delta * input
-    weight -= weight_delta * alpha
+    weight = weight - weight_delta
+    print('Error', error, ' Prediction', prediction)
+    print('Delta', str(delta), ' Weight Delta', weight_delta)
