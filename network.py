a8e6d5fb8da1bad38d2db09b9c8aee68743453d3
arun <arun@layeriv.com>
2020-12-06 21:51:53 +0530

    single input to multiple output gradient descent

--- network.py
+++ network.py
@@ -5,34 +5,40 @@ def w_sum(input, weights):
         output += input[i] * weights[i]
     return output
 
-def neural_network(input, weights):
-    prediction = w_sum(input, weights)
-    return prediction
-
 def elementwise_multiplication(number, vector):
     output = []
     for i in range(len(vector)):
         output.append(vector[i]* number)
     return output
 
-weights = [0.1, 0.2, -.1]
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
-win_or_lose_binary = [1, 1, 0, 1]
-true = win_or_lose_binary[0]
-input = [toes[0], wlrec[0], nfans[0]]
+def neural_network(input, weights):
+    prediction = elementwise_multiplication(input, weights)
+    return prediction
 
-for iteration in range(100000):
+weights = [0.3, 0.2, 0.9,]
+alpha = 0.1
+wlrec = [0.65, 1.0, 1.0, 0.9]
+
+hurt = [0.1, 0.0, 0.0, 0.1]
+win = [ 1, 1, 0, 1]
+sad = [0.1, 0.0, 0.1, 0.2]
+
+true = [hurt[0], win[0], sad[0]]
+input = wlrec[0]
+for iteration in range(100):
     prediction = neural_network(input, weights)
-    error = (prediction - true)**2
-    delta = prediction - true
-    weight_deltas = elementwise_multiplication(delta, weights)
-    alpha = 0.01
+    deltas = []
+    errors = []
+    for i in range(len(true)):
+        errors.append((prediction[i] - true[i])**2)
+        deltas.append(prediction[i] - true[i])
 
-    for i in range(len(weight_deltas)):
-        weights[i] -= alpha * weight_deltas[i]
+    weight_deltas = elementwise_multiplication(input, deltas)
 
-    print('-------\nError', str(error), "Prediction", prediction)
-    print("Weights", weights)
-    print("Weight Deltas", weight_deltas)
+    for i in range(len(weights)):
+        weights[i] -= weight_deltas[i] * alpha
+
+    print('--------\nErrors', errors, 'Prediction', prediction)
+    # print(deltas)
+    print('weight_deltas', weight_deltas)
+    # print(weights)
