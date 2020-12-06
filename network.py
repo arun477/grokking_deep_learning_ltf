700cf8288021f4bc08fd6ea78d22fd1a75639506
arun <arun@layeriv.com>
2020-12-06 21:08:16 +0530

    mulitple input and single output gradient descent

--- network.py
+++ network.py
@@ -1,28 +1,38 @@
-import numpy as np
+def w_sum(input, weights):
+    assert(len(input) == len(weights))
+    output = 0
+    for i in range(len(input)):
+        output += input[i] * weights[i]
+    return output
 
-def neural_network(inputs, weights):
-    hidden = inputs.dot(weights[0])
-    prediction = hidden.dot(weights[1])
+def neural_network(input, weights):
+    prediction = w_sum(input, weights)
     return prediction
 
-#hidden network weight
-ih_wgt = np.array([ [0.1, 0.2, -0.1],
-           [-0.1,0.1, 0.9], 
-           [0.1, 0.4, 0.1] ] )
+def elementwise_multiplication(number, vector):
+    output = []
+    for i in range(len(vector)):
+        output.append(vector[i]* number)
+    return output
 
-#output network weight
-hp_wgt = np.array([ [0.3, 1.1, -0.3], 
-           [0.1, 0.2, 0.0], 
-           [0.0, 1.3, 0.1] ])
-
-weights = [ih_wgt, hp_wgt]
-
+weights = [0.1, 0.2, -.1]
 toes = [8.5, 9.5, 9.9, 9.0]
 wlrec = [0.65, 0.8, 0.8, 0.9]
 nfans = [1.2, 1.3, 0.5, 1.0]
+win_or_lose_binary = [1, 1, 0, 1]
+true = win_or_lose_binary[0]
+input = [toes[0], wlrec[0], nfans[0]]
 
-inputs = np.array([toes[0], wlrec[0], nfans[0]])
-prediction = neural_network(inputs, weights)
-print(prediction)
+for iteration in range(100000):
+    prediction = neural_network(input, weights)
+    error = (prediction - true)**2
+    delta = prediction - true
+    weight_deltas = elementwise_multiplication(delta, weights)
+    alpha = 0.01
 
-[0.21350000000000002, 0.14500000000000002, 0.5065]
+    for i in range(len(weight_deltas)):
+        weights[i] -= alpha * weight_deltas[i]
+
+    print('-------\nError', str(error), "Prediction", prediction)
+    print("Weights", weights)
+    print("Weight Deltas", weight_deltas)
