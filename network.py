464f65e643b545b32efe398f01d35d680a8e0ba9
arun <arun@layeriv.com>
2020-12-06 16:45:15 +0530

    staked neural network

--- network.py
+++ network.py
@@ -14,19 +14,26 @@ def vector_matrix_multiplictation(vect, matrix):
     return output
 
 def neural_network(inputs, weights):
-    prediction = vector_matrix_multiplictation(inputs, weights)
+    hidden = vector_matrix_multiplictation(inputs, weights[0])
+    prediction = vector_matrix_multiplictation(hidden, weights[1])
     return prediction
 
-# mxn = 3*3
-weights = [[0.1, 0.1, -0.3],
-           [0.1, 0.2, 0.0],
-           [0.0, 1.3, 0.1]]
+#hidden network weight
+ih_wgt = [ [0.1, 0.2, -0.1],
+           [-0.1,0.1, 0.9], 
+           [0.1, 0.4, 0.1] ] 
+
+#output network weight
+hp_wgt = [ [0.3, 1.1, -0.3], 
+           [0.1, 0.2, 0.0], 
+           [0.0, 1.3, 0.1] ] 
+
+weights = [ih_wgt, hp_wgt]
 
 toes = [8.5, 9.5, 9.9, 9.0]
 wlrec = [0.65, 0.8, 0.8, 0.9]
 nfans = [1.2, 1.3, 0.5, 1.0]
 
-# mxn = 1*3
 inputs = [toes[0], wlrec[0], nfans[0]]
 prediction = neural_network(inputs, weights)
 print(prediction)
