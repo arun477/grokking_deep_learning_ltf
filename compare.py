4e0287617e53bc797df8957f4a60958faaa4e8c7
arun <arun@layeriv.com>
2020-12-06 17:54:21 +0530

    simple hot and cold learning for 1101 iteration

--- compare.py
+++ compare.py
@@ -1,22 +1,21 @@
 weight = 0.5
 input = 0.5
-correct_prediction = 0.8
-lr = 0.01
+goal_prediction = 0.8
 
-def neural_network(input, weight):
-    prediction  = input * weight
-    return prediction
-prediction = neural_network(input, weight)   
-error = (prediction - correct_prediction)**2
+step_amount = 0.001
 
+for iteration in range(1101):
+    prediction = input * weight
+    error = (goal_prediction - prediction)**2
+    print('Error',  str(error), 'Prediction', prediction)
 
+    up_direction = input * (weight + step_amount)
+    up_error =  (goal_prediction - up_direction)**2
 
-p_up = neural_network(input, weight+lr)
-e_up = (p_up - correct_prediction)**2
+    down_prediction = input * (weight - step_amount)
+    down_error = (goal_prediction - down_prediction)**2
 
-p_down = neural_network(input, weight-lr)
-e_down = (p_down - correct_prediction)**2
-
-
-print('error up', e_up)
-print('error down', e_down)
+    if (down_error < up_error):
+        weight = weight - step_amount
+    if (down_error > up_error):
+        weight = weight + step_amount
