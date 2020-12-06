ceef495943b0b96d116dc4a8c8308f721585f30b
arun <arun@layeriv.com>
2020-12-06 18:43:24 +0530

    100 iteration of gradient descent

--- compare.py
+++ compare.py
@@ -1,10 +1,17 @@
-weight = 0.5
-input = 0.5
-goal_prediction = 0.8
+weight = 0.1
+alpha = 0.01
 
-for iteration in range(20):
+
+num_of_toes = [8.5]
+win_or_lose_binary = [1]
+
+input = num_of_toes[0]
+goal_prediction = win_or_lose_binary[0]
+
+for iteration in range(100):
     prediction = input * weight
-    error = (goal_prediction - prediction)**2
-    direction_and_amount = (prediction - goal_prediction) * input
-    weight = weight - direction_and_amount
-    print('Error',  str(error), 'Prediction', prediction)
+    error = (prediction - goal_prediction)**2
+    print('Error', str(error), 'Prediction', prediction)
+    delta = prediction - goal_prediction
+    weight_delta = delta * input
+    weight -= weight_delta * alpha
