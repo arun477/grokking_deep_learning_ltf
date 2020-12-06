5a77c21048d48f8b78b9c9c991c58881d0c5f8c6
arun <arun@layeriv.com>
2020-12-06 16:40:52 +0530

    multiple input and multiple output

--- network.py
+++ network.py
@@ -1,16 +1,32 @@
-def elementwise_multiplication(number, vector):
-    output = []
-    for i in range(len(vector)):
-        output.append(number * vector[i])
+def w_sum(a, b):
+    assert(len(a) == len(b))
+    output = 0
+    for i in range(len(a)):
+        output += (a[i] * b[i])
     return output
 
-def neural_network(weights, input):
-    prediction  = elementwise_multiplication(input, weights)
+
+def vector_matrix_multiplictation(vect, matrix):
+    assert(len(vect) == len(matrix))
+    output = []
+    for i in range(len(matrix)):
+        output.append(w_sum(vect, matrix[i]))
+    return output
+
+def neural_network(inputs, weights):
+    prediction = vector_matrix_multiplictation(inputs, weights)
     return prediction
 
-weights = [0.3, 0.2, 0.9]
-wins_loss = [0.65, 0.8, 0.8, 0.9]
+# mxn = 3*3
+weights = [[0.1, 0.1, -0.3],
+           [0.1, 0.2, 0.0],
+           [0.0, 1.3, 0.1]]
 
-input = wins_loss[0]
-prediction = neural_network(weights, input)
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+# mxn = 1*3
+inputs = [toes[0], wlrec[0], nfans[0]]
+prediction = neural_network(inputs, weights)
 print(prediction)
