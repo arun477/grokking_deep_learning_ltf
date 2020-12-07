71740a1d3de5d865d3871b805da5727a95cfb743
arun <arun@layeriv.com>
2020-12-07 23:04:32 +0530

    stochastic gradient

--- street_light_problem.py
+++ street_light_problem.py
@@ -9,23 +9,22 @@ streetlights = np.array([
 ])
 
 walk_vs_stop = np.array([
-    [ 0 ],
-    [ 1 ],
-    [ 0 ],
-    [ 1 ],
-    [ 1 ],
-    [ 0 ]
+    0, 1, 0, 1, 1, 0
 ])
 
-inputs = streetlights[0]
-goal_of_prediction = walk_vs_stop[0]
+
 alpha = 0.1
 weights = np.array([0.5,0.48,-0.7])
 
-for iteration in range(5000):
-    prediction = inputs.dot(weights)
-    error = (prediction - goal_of_prediction) ** 2
-    delta = prediction - goal_of_prediction
-    weights_delta = inputs * delta
-    weights = weights - (weights_delta * alpha)
-    print('-----\nError', error, 'Prediction', prediction)
+for iteration in range(20):
+    total_error = 0
+    for row_index in range(len(streetlights)):
+        inputs = streetlights[row_index]
+        goal_of_prediction = walk_vs_stop[row_index]
+        prediction = inputs.dot(weights)
+        error = (prediction - goal_of_prediction) ** 2
+        total_error += error
+        delta = prediction - goal_of_prediction
+        weights_delta = inputs * delta
+        weights = weights - (weights_delta * alpha)
+    print('-----\nError Total', total_error)
